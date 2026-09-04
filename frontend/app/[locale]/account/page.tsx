2c3e8599db7199e261f809818229b439af164515
NAWARXVOND <nhxg369@gmail.com>
2026-09-04 21:44:46 +0400

    feat: show profile details on account page

--- frontend/app/[locale]/account/page.tsx
+++ frontend/app/[locale]/account/page.tsx
@@ -1,9 +1,13 @@
 import { notFound } from "next/navigation";
 import { AccountView } from "@/components/account-view";
+import { ProfileDetailsCard } from "@/components/profile-details";
 import { isLocale } from "@/lib/i18n";
 
 export default async function AccountPage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   if (!isLocale(locale)) notFound();
-  return <AccountView locale={locale} />;
+  return <>
+    <ProfileDetailsCard locale={locale} />
+    <AccountView locale={locale} />
+  </>;
 }
